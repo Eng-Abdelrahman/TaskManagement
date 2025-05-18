b2c8843bc30f665789ef177281151da91d8580e5
Abdelrahman Mahmoud <eng.abdelrahman1995@hotmail.com>
2025-05-18 13:37:55 +0400

    fix Toast issue

--- taskManagementApp/src/App.tsx
+++ taskManagementApp/src/App.tsx
@@ -2,10 +2,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "react-toastify/dist/ReactToastify.css";
 
 import TaskList from "./pages/TaskList";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
-   <TaskList />
+   <><TaskList /><ToastContainer position="top-right" autoClose={3000} hideProgressBar /></>
   );
 }
 
